99b13dc9cae25c2edd631cfa317ff96371e1e969
yeswanthchirumamilla <140904130+yeswanthchirumamilla@users.noreply.github.com>
2025-01-09 23:39:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import searchRoutes from "./routes/search.routes.js";
+import requestRoutes from "./routes/request.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
@@ -18,13 +21,15 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
+app.use(cors()); // Enable CORS
 
 app.use("/api/auth", authRoutes);
+app.use("/api/search", searchRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/request", requestRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
