0bfe5dc5253dc9430324f1b6b15aab7027807514
yeswanthchirumamilla <140904130+yeswanthchirumamilla@users.noreply.github.com>
2025-01-09 23:27:58 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["https://chat-app-wlia.onrender.com/login"],
+		origin: ["https://chat-app-wlia.onrender.com"],
 		methods: ["GET", "POST"],
 	},
 });
