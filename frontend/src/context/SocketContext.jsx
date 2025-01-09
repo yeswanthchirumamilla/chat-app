cfa6829f3d25a0648f380f3eef0ac11249cc5f57
yeswanthchirumamilla <140904130+yeswanthchirumamilla@users.noreply.github.com>
2025-01-09 23:28:34 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-wlia.onrender.com/login", {
+			const socket = io("https://chat-app-wlia.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
