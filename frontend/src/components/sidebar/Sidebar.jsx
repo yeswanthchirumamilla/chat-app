b95c13102d6eb303f8106c67ef0822fd56a9f38f
chirumamilla yeswanth <yaswanthch97@gmail.com>
2025-01-10 10:27:52 +0530

    Update Sidebar.jsx

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,16 +1,18 @@
+import React from "react";
 import Conversations from "./Conversations";
 import LogoutButton from "./LogoutButton";
 import "./Sidebar.css";
 import SearchInput from "./SearchInput";
 
-const Sidebar = () => {
-	return (
-		<div className="sidebar">
-			
-			<div className="divider px-3"></div>
-			<Conversations />
-			<LogoutButton />
-		</div>
-	);
+const Sidebar = ({ onConversationSelect }) => {
+  return (
+    <div className="sidebar">
+      <SearchInput />
+      <div className="divider px-3"></div>
+      <Conversations onConversationSelect={onConversationSelect} />
+      <LogoutButton />
+    </div>
+  );
 };
+
 export default Sidebar;
