afa7230eb51d58e9df7e009f2d62276d00c50568
yeswanthchirumamilla <yaswanthch97@gmail.com>
2025-01-11 10:20:37 +0530

    Revert "Update Sidebar.jsx"
    This reverts commit b95c13102d6eb303f8106c67ef0822fd56a9f38f.

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,18 +1,16 @@
-import React from "react";
 import Conversations from "./Conversations";
 import LogoutButton from "./LogoutButton";
 import "./Sidebar.css";
 import SearchInput from "./SearchInput";
 
-const Sidebar = ({ onConversationSelect }) => {
-  return (
-    <div className="sidebar">
-      <SearchInput />
-      <div className="divider px-3"></div>
-      <Conversations onConversationSelect={onConversationSelect} />
-      <LogoutButton />
-    </div>
-  );
+const Sidebar = () => {
+	return (
+		<div className="sidebar">
+			
+			<div className="divider px-3"></div>
+			<Conversations />
+			<LogoutButton />
+		</div>
+	);
 };
-
 export default Sidebar;
