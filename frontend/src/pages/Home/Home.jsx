6ded7e1616520a49259ccda950cc61b34c76ffb5
yeswanthchirumamilla <yaswanthch97@gmail.com>
2025-01-11 10:20:43 +0530

    Revert "Update Home.jsx"
    This reverts commit 088c3dfc17ec2ebd90d241ebba512a8895a0ae61.

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import useLogout from "../../hooks/useLogout";
 import { useNavigate } from "react-router-dom";
@@ -59,16 +59,7 @@ const Home = () => {
     setSelectedConversation(null);
   };
 
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  const isMobile = window.innerWidth <= 768;
 
   return (
     <div className="chat-app">
